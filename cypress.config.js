4217931cff0253bab645b648441031caa98e02db
dhruvjadav74 <jadav.dhruv.74@gmail.com>
2024-03-08 16:44:29 +0530

    added projectID to config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "itiei1",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
